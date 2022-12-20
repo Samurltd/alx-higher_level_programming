ddc36199db2af25b837b88343b244555fed0f357
Ben Samuel <bensmoothsamel@gmail.com>
2022-12-20 22:41:34 +0000

    Write the Python class MagicClass that does exactly the same as the following Python bytecode:

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,23 +1,21 @@
-
 #!/usr/bin/python3
-"""writing a docstring"""
+"""Class MagicClass"""
 import math
 
 
 class MagicClass:
-    """set up the magic"""
-
+    """Defines MagicClass"""
     def __init__(self, radius=0):
-        """ writing another docstring """
+        """Initializes Data"""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
-            raise TypeError('radius must be a number')
+            raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
-        """again with the docstring"""
-        return self.__radius ** 2 * math.pi
+        """Get area"""
+        return (self.__radius ** 2) * math.pi
 
     def circumference(self):
-        """such docstring"""
-        return 2 * math.pi * self.__radius
+        """Get Circumference"""
+        return (2 * math.pi * self.__radius)
