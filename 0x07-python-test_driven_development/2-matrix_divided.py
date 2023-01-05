8e09bca2b21643e93280060f74b89e83e575be38
Ben Samuel <bensmoothsamel@gmail.com>
2023-01-05 20:27:08 +0000

    Write a function that divides all elements of a matrix.
    Prototype: def matrix_divided(matrix, div):
    matrix must be a list of lists of integers or floats, otherwise raise a TypeError exception with the message matrix must be a matrix (list of lists) of integers/floats
    Each row of the matrix must be of the same size, otherwise raise a TypeError exception with the message Each row of the matrix must have the same size
    div must be a number (integer or float), otherwise raise a TypeError exception with the message div must be a number
    div can’t be equal to 0, otherwise raise a ZeroDivisionError exception with the message division by zero
    All elements of the matrix should be divided by div, rounded to 2 decimal places
    Returns a new matrix
    You are not allowed to import any module

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,37 +1,36 @@
-
 #!/usr/bin/python3
+
+
 """
-This module defines a matrix division function
+A function that performs magic
 """
 
 
 def matrix_divided(matrix, div):
-    """This function divides all elements of a matrix by a given number
-    Args:
-        matrix: A list of lists (matrix)- members can be of type ints or floats
-        div: Number to be used for the division (can be a float or an integer)
-    Raises:
-        TypeError: If the matrix contains non-numbers
-        TypeError: If the matrix contains rows of different sizes
-        TypeError: If div is not an int or float
-        ZeroDivisionError: If div is 0
-    Returns:
-        A new matrix which represents the result of the divisions
     """
-    if (not isinstance(matrix, list) or matrix == [] or
-            not all(isinstance(row, list) for row in matrix) or
-            not all((isinstance(ele, int) or isinstance(ele, float))
-                    for ele in [num for row in matrix for num in row])):
-        raise TypeError("matrix must be a matrix (list of lists) of "
-                        "integers/floats")
+    A function that divides all element of a
+    matrix by div and returns a nested list
+    """
 
-    if not all(len(row) == len(matrix[0]) for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
+    msg = "matrix must be a matrix (list of lists) of integers/floats"
 
-    if not isinstance(div, int) and not isinstance(div, float):
+    for i in matrix:
+        length = len(i)
+        for j in i:
+            if type(j) not in [int, float]:
+                raise TypeError(msg)
+
+    for k in matrix:
+        if len(k) != length:
+            msg = "Each row of the matrix must have the same size"
+            raise TypeError(msg)
+
+    if type(div) not in [int, float]:
         raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
+    new = [[float("{:.2f}".format(a / div)) for a in i] for i in matrix]
+
+    return (new)
