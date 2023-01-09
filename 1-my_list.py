441fbc4a976737b196e3f151e8a046d7d0e699d4
Ben Sam <bensmoothsamel@gmail.com>
2023-01-09 13:55:17 -0800

    update

--- 1-my_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""A module that deals with inheritance"""
-
-
-class MyList(list):
-    """An inherited class"""
-    def print_sorted(self):
-        new = sorted(self)
-        print(new)
