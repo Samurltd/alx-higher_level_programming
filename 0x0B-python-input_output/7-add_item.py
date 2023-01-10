83d804dc6329e481838656e9e50a830e3198a8b4
Ben Samuel <bensmoothsamel@gmail.com>
2023-01-10 18:47:41 +0000

    Write a script that adds all arguments to a Python list, and then save them to a file:
    You must use your function save_to_json_file from 5-save_to_json_file.py
    You must use your function load_from_json_file from 6-load_from_json_file.py
    The list must be saved as a JSON representation in a file named add_item.json
    If the file doesn’t exist, it should be created
    You don’t need to manage file permissions / exceptions.

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Copyright 2023 © ben"""
+
+import json
+import os
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+list_to_save = []
+for i in sys.argv:
+    list_to_save.append(i)
+if len(list_to_save) >= 1:
+    list_to_save.pop(0)
+
+if os.path.exists('add_item.json'):
+    data = load_from_json_file('add_item.json')
+    for i in list_to_save:
+        data.append(i)
+    save_to_json_file(data, 'add_item.json')
+
+else:
+    save_to_json_file(list_to_save, 'add_item.json')
